47b3deda9c06baffc6a915afef08cb1cca344f8c
PiyushIncognito <piyushchaudhari0514@gmail.com>
2022-05-08 21:05:02 +0530

    More Programs added

--- RecursionFactorial.java
+++ RecursionFactorial.java
@@ -1,2 +1,19 @@
-package com.javalove;public class RecursionFactorial {
+package com.javalove;
+
+public class RecursionFactorial {
+    public static int printfact(int n){
+        if(n==1 || n==0){
+            return 1;
+        }
+        int fact_num = printfact(n-1);
+        int fact = n * fact_num;
+        return fact;
+
+    }
+
+    public static void main(String[] args) {
+        int n = 4;
+        int ans = printfact(n);
+        System.out.println(ans);
+    }
 }
--- RecursionReverse.java
+++ RecursionReverse.java
@@ -1,2 +1,16 @@
-package com.javalove;public class RecursionReverse {
+package com.javalove;
+
+public class RecursionReverse {
+    public static void reverse(String str, int idx){
+        if(idx ==0){
+            System.out.println(str.charAt(idx));
+            return;
+        }
+        System.out.print(str.charAt(idx));
+        reverse(str, idx -1);
+    }
+    public static void main(String[] args) {
+        String str = "abcd";
+        reverse(str, str.length()-1);
+    }
 }
